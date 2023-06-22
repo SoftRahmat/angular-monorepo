731485d317a3f1e8c9e7305f172812f71d4150f8
SoftRahmat <rahmatapon402@gmail.com>
2023-06-22 17:39:47 +0800

    BE changes

---
+++ apps/ngshop/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import{UiModule} from '@bluesky/ui';
+
+// for routing pages
+// here type Routes is importing from angular router
+// for type of const is importam for typescript
+const routes: Routes = [
+    { path: '', component: HomePageComponent },
+    { path: 'products', component: ProductListComponent }
+];
+
+@NgModule({
+    declarations: [AppComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
+    imports: [BrowserModule, RouterModule.forRoot(routes), UiModule],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {}
